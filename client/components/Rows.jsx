7e03a05073d9b1f3c5c021ac30ecfdad0cf6a56c
WilliamHaakLee <leewilli12@gmail.com>
2022-12-28 15:54:15 -0600

    Finish the barebones for the game components
    Co-authored-by: Chris Oakes <ckoakes1@gmail.com>

--- client/components/Rows.jsx
+++ client/components/Rows.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
-import BoxCreator from "./BoxCreator.jsx"
+import Box from "./Box.jsx"
 
 const Rows = props => {
+  const {id, enemyShipLocations, setEnemyShipLocations, playerMisses, setPlayerMisses, playerHits, setPlayerHits} = props;
+  const boxArr = [];
+  for (let i = 1; i <= 5; i++) {
+  boxArr.push(<Box
+    
+    id = {`${id}${i}`}
+    enemyShipLocations = {enemyShipLocations} 
+    setEnemyShipLocations = {setEnemyShipLocations}
+    playerMisses = {playerMisses}
+    setPlayerMisses = {setPlayerMisses}
+    playerHits = {playerHits}
+    setPlayerHits = {setPlayerHits}
+  />)
+  }
+
   return (
-    <div>Rows:<BoxCreator /></div>
+    <div>
+      {boxArr}
+    </div>
   )
 }
 
